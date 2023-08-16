19d0d318734ce56c26389e9f4738af6ae34557f8
Mark View <asir64@163.com>
2023-08-16 13:12:16 +0800

    Deploy

--- .temp/internal/pagesData.js
+++ .temp/internal/pagesData.js
@@ -5,14 +5,14 @@ export const pagesData = {
   "v-13925e64": () => import(/* webpackChunkName: "v-13925e64" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Test/index.html.js").then(({ data }) => data),
   // path: /Test/Test.html
   "v-634ab983": () => import(/* webpackChunkName: "v-634ab983" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Test/Test.html.js").then(({ data }) => data),
-  // path: /Thoughts/
-  "v-d48baa08": () => import(/* webpackChunkName: "v-d48baa08" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Thoughts/index.html.js").then(({ data }) => data),
   // path: /Review/Meizu%2020%20Review.html
   "v-4bde4e9e": () => import(/* webpackChunkName: "v-4bde4e9e" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Review/Meizu 20 Review.html.js").then(({ data }) => data),
   // path: /Review/
   "v-0ecdfe9e": () => import(/* webpackChunkName: "v-0ecdfe9e" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Review/index.html.js").then(({ data }) => data),
   // path: /Review/iOS%2017%20_%20SF%20Symbols%205%20Review.html
   "v-7baef728": () => import(/* webpackChunkName: "v-7baef728" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Review/iOS 17 _ SF Symbols 5 Review.html.js").then(({ data }) => data),
+  // path: /Thoughts/
+  "v-d48baa08": () => import(/* webpackChunkName: "v-d48baa08" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/Thoughts/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/asir/vuepress-starter/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
 }
--- .temp/internal/pagesRoutes.js
+++ .temp/internal/pagesRoutes.js
@@ -2,9 +2,9 @@ export const pagesRoutes = [
   ["v-8daa1a0e","/",{"title":"Hi there!"},["/README.md"]],
   ["v-13925e64","/Test/",{"title":"README.md Here's a testing folder includes all .md files for testing."},["/Test/README.md"]],
   ["v-634ab983","/Test/Test.html",{"title":"Test.md (aka Test.html)"},[":md"]],
-  ["v-d48baa08","/Thoughts/",{"title":"Thoughts, Full of Ideas."},["/Thoughts/README.md"]],
   ["v-4bde4e9e","/Review/Meizu%2020%20Review.html",{"title":"📱 Meizu 20 Review"},["/Review/Meizu 20 Review.html","/Review/Meizu 20 Review.md",":md"]],
   ["v-0ecdfe9e","/Review/",{"title":"Reviews"},["/Review/README.md"]],
   ["v-7baef728","/Review/iOS%2017%20_%20SF%20Symbols%205%20Review.html",{"title":"📱 iOS 17 & SF Symbols 5 Review"},["/Review/iOS 17 _ SF Symbols 5 Review.html","/Review/iOS 17 & SF Symbols 5 Review.html","/Review/iOS%2017%20&%20SF%20Symbols%205%20Review.html","/Review/iOS 17 & SF Symbols 5 Review.md","/Review/iOS%2017%20&%20SF%20Symbols%205%20Review.md"]],
+  ["v-d48baa08","/Thoughts/",{"title":"Thoughts, Full of Ideas."},["/Thoughts/README.md"]],
   ["v-3706649a","/404.html",{"title":""},[]],
 ]
