29b5ae17f7b8e04e66b140de55731f58557790e0
Mark View <asir64@163.com>
2023-08-16 13:05:03 +0800

    Deploy

--- .temp/pages/index.html.vue
+++ .temp/pages/index.html.vue
@@ -1,5 +1,8 @@
-<template><div><h1 id="hello-vuepress" tabindex="-1"><a class="header-anchor" href="#hello-vuepress" aria-hidden="true">#</a> Hello VuePress</h1>
-<p><strong>Hello World!</strong> This is the <code v-pre>README.md</code> file under <code v-pre>/docs/</code> path, which is the default path for all documents.</p>
+<template><div><h1 id="hi-there" tabindex="-1"><a class="header-anchor" href="#hi-there" aria-hidden="true">#</a> Hi there!</h1>
+<h2 id="here-s-mark-view-s-blog" tabindex="-1"><a class="header-anchor" href="#here-s-mark-view-s-blog" aria-hidden="true">#</a> Here's Mark View's blog.</h2>
+<p>Check out my works at... <a href="https://github.com/asir2004" target="_blank" rel="noopener noreferrer">GitHub<ExternalLinkIcon/></a> or <a href="https://youtube.com/@MarkView64" target="_blank" rel="noopener noreferrer">YouTube<ExternalLinkIcon/></a></p>
+<h2 id="self-introduction" tabindex="-1"><a class="header-anchor" href="#self-introduction" aria-hidden="true">#</a> Self Introduction</h2>
+<p>Hi, I'm Mark. I haven't thought about my self-given last name yet, so 'View' became my temporary last name on the Internet. I love motion design, and exploring about technology. So you'll see some of tech products reviews of mine here.</p>
 </div></template>
 
 
