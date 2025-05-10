c43d760b8fe1ee2dd9d80c77559f59cbeee0c5a3
asir2004 <smallwhite2017@gmail.com>
2025-05-11 01:19:17 +0800

    Fixed typo

--- src/data/yearlyDescriptions.ts
+++ src/data/yearlyDescriptions.ts
@@ -1,5 +1,5 @@
 export const yearlyDescriptions: { [key: number]: string } = {
-    2025: "Road goes on...",
+    2025: "",
     2024: "更加成熟、独立，更具挑战性的项目，新认识了一些海内外客户和朋友",
     2023: "逐渐向动效设计师靠拢",
     2022: "高考之年，焦虑迷茫",
