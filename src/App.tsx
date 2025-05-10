5ea5a4f4ef48accefb1933026ff3cb3b44fc7b9f
asir2004 <smallwhite2017@gmail.com>
2025-05-10 14:40:51 +0800

    Rolled routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { ProjectsPage, ContactPage, ProcessPage } from './pages'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 
 function App() {
