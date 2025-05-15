e8735f33466329cbe0c516916dd5c0f857204e2d
asir2004 <smallwhite2017@gmail.com>
2025-05-16 00:33:21 +0800

    Changed nav router from HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { ProjectsPage, ContactPage, ProcessPage, MainPage } from './pages'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
           <Route path="/projects/:projectId" element={<ProjectsPage />} />
           <Route path="/contact/" element={<ContactPage />} />
           <Route path="/process/" element={<ProcessPage />} />
+          <Route path="*" element={<MainPage />}></Route>
         </Routes>
       </div>
     </Router>
